c52e35ddef98220701cc80cab236ee3366297a52
AFF40 <101578368+AFF40@users.noreply.github.com>
2022-09-07 10:28:43 -0400

    Add files via upload

---
+++ examen1.java
@@ -0,0 +1,14 @@
+package com.sotec.examen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class examen1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_examen1);
+    }
+}
